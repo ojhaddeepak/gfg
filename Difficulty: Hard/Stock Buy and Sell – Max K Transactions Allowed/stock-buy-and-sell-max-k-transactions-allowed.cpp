992da4e6085323f1d0088ab886610ffc8d066b39
ojhaddeepak <116076273+ojhaddeepak@users.noreply.github.com>
2025-03-19 22:38:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Stock Buy and Sell – Max K Transactions Allowed/stock-buy-and-sell-max-k-transactions-allowed.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int maxProfit(vector<int>& prices, int k) {
+        int n = prices.size();
+        if (n == 0) return 0;
+
+        // If k >= n/2, it's equivalent to unlimited transactions
+        if (k >= n / 2) {
+            int profit = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i - 1])
+                    profit += prices[i] - prices[i - 1];
+            }
+            return profit;
+        }
+
+        // DP table
+        vector<vector<int>> dp(k + 1, vector<int>(n, 0));
+
+        // Fill DP table
+        for (int i = 1; i <= k; i++) {
+            int maxDiff = -prices[0];
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = max(dp[i][j - 1], prices[j] + maxDiff);
+                maxDiff = max(maxDiff, dp[i - 1][j] - prices[j]);
+            }
+        }
+        return dp[k][n - 1];
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        getline(cin, input);
+        istringstream iss(input);
+        vector<int> arr;
+        int num;
+
+        // Read integers from the input string
+        while (iss >> num) {
+            arr.push_back(num);
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+        Solution ob;
+        cout << ob.maxProfit(arr, k) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
