86de958e284c1180da5cb41ccae4dc81580c91db
ojhaddeepak <116076273+ojhaddeepak@users.noreply.github.com>
2025-04-06 18:13:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+
+class Solution {
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        int n=edges.size();
+        
+        vector<vector<int>>adj(V);
+        vector<int>indeg(V,0);
+        
+        for(int i=0;i<n;i++)
+        {
+            adj[edges[i][0]].push_back(edges[i][1]);
+            
+            indeg[edges[i][1]]++;
+        }
+        
+        queue<int>q;
+        for(int i=0;i<V;i++)
+        {
+            if(indeg[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        
+        vector<int>ans;
+        while(!q.empty())
+        {
+            int temp=q.front();
+            q.pop();
+            
+            ans.push_back(temp);
+            for(auto it:adj[temp])
+            {
+                indeg[it]--;
+                if(indeg[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+//GFG POTD solution for 06 April
+
+
+
+//{ Driver Code Starts.
+
+int check(int V, vector<int> &res, vector<vector<int>> adj) {
+
+    if (V != res.size())
+        return 0;
+
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v])
+                return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int V, E;
+        cin >> V >> E;
+
+        int x = V;
+        vector<vector<int>> adj(V);
+        vector<vector<int>> edges;
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            edges.push_back({u, v});
+        }
+
+        Solution obj;
+        vector<int> res = obj.topoSort(V, edges);
+        bool ans = check(x, res, adj);
+        if (ans)
+            cout << "true\n";
+        else
+            cout << "false\n";
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
